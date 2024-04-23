b423b53e9f3774d2b97b431df30725474864eeef
ruby2048 <dkssudtmdwls@naver.com>
2024-04-23 16:31:29 +0900

    Midterm Test end
    finish

---
+++ Book.java
@@ -0,0 +1,8 @@
+public interface Book {
+	String getIsbn();
+	String getTitle();
+	String getAuthor();
+	int getYear();
+	boolean isAvailable();
+	void setAvailable(boolean available);
+}
--- LibrarySystem.java
+++ LibrarySystem.java
@@ -1,11 +1,14 @@
 public class LibrarySystem {
-	// 변수 정의
+	private Book[] booklist;
+	private static int bookCount = 0;
+	private static final int NUM_BOOK = 10;
 	
-	// 생성자 정
-
+	public LibrarySystem() {
+	    this.booklist = new Book[NUM_BOOK]; // 생성자
+	}
+	
 	public void addBook(Book book) {
-		// 책 추가
-		// 도움을 위해 Cart.java의 insertBook(Book book)을 참조하세요
+		booklist[bookCount++] = new LibraryBook(book);
 	}
 	
 	public void removeBook(String title) {
@@ -23,7 +26,7 @@ public class LibrarySystem {
 				// 마지막 요소를 null로 설정하고 bookCount를 줄입니다.
 				booklist[--bookCount] = null;
 				System.out.println("라이브러리에서 '" + title + "' 삭제했습니다.");
-        
+				
 				return; // 책 제거 후 종료 방법
 			}
 		}
@@ -32,46 +35,56 @@ public class LibrarySystem {
 	}
 	
 	public void borrowBook(String title) {
-        // `for` LibrarySystem의 모든 Book {
-            // `if` 책 제목이 발견되면 {
-                // `if` 책이 있다면 {
-                    // setAvailable을 false로 설정하고 메시지를 출력합니다.
-				// } `else` 책이 없다면 {
-					// 메시지를 인쇄하다
-				// }
+		// `for` LibrarySystem의 모든 Book
+		for ( int i = 0; i < bookCount; i++ ) {
+  		
+			// `if` 책 제목이 발견되면
+			if ( booklist[i].getTitle().equals(title)) {
+				
+				if (booklist[i].isAvailable()) {
+					booklist[i].setAvailable(false);
+					System.out.println(booklist[i].getTitle() + "빌림");
+				}
+				else {
+					System.out.println("이미 빌린 상태");
+				}
 				return; // 책 제거 후 종료 방법
-			// }
-		// }
+			}
+		}
 		// 찾을 수 없으면 메시지를 인쇄합니다.
 		System.out.println("# 오류: '" + title + "' 못 찾았습니다!");
 	}
 	
 	public void returnBook(String title) {
-	    // `for` LibrarySystem의 모든 Book {
-            // `if` 책 제목이 발견되면 {
-                // `if` 책이 없다면 {
-                    // setAvailable을 true로 설정하고 메시지를 출력합니다.
-				// } `else` 책이 있다면 {
-					// 메시지를 인쇄하다
-				// }
+		// `for` LibrarySystem의 모든 Book
+		for ( int i = 0; i < bookCount; i++ ) {
+	  		
+			// `if` 책 제목이 발견되면
+			if ( booklist[i].getTitle().equals(title)) {
+				
+				if (!booklist[i].isAvailable()) {
+					booklist[i].setAvailable(true);
+					System.out.println(booklist[i].getTitle() + "반납");
+				}
+				else {
+					System.out.println("이미 반납된 상태");
+				}
 				return; // 책 제거 후 종료 방법
-			// }
-		// }
+			}
+		}
 		// 찾을 수 없으면 메시지를 인쇄합니다.
 		System.out.println("# 오류: '" + title + "' 못 찾았습니다!");
 	}
 	
 	public void displayAllBooks() {
-		// 책 목록 출력 (책이름, 저자, 출판년도, 대출가능여부 출력)
-
-        /* 
-        Expected output:
-        "ISBN1234 | JSP 웹 프로그래밍 | 송미영 | 2018 | Available"
-        "ISBN1235 | 안드로이드 프로그래밍 | 우재남 | 2022 | Out"
-        ...
-        */
-    
-        // 도움을 위해 Cart.java의 printBookList()를 참조하세요
+		for (int i = 0; i < bookCount; i++) {
+			System.out.print(booklist[i].getIsbn() + "|");
+			System.out.print(booklist[i].getTitle() + "|");
+			System.out.print(booklist[i].getAuthor() + "|");
+			System.out.print(booklist[i].getYear() + "|");
+			System.out.print(booklist[i].isAvailable() + "|");
+			System.out.println("");
+		}
     }
 
 }
